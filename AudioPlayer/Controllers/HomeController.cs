961cdde53e47aa97f5b21249f0e2f638b3476459
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-23 09:30:09 +0700

    feat(HomeController): add ApplicationContext

---
+++ AudioPlayer/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using AudioPlayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AudioPlayer.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ApplicationContext _db = new ();
+    
+    public IActionResult Index()
+    {
+        var context = new ApplicationContext();
+        var t = context.Database;
+        return View();
+    }
+}
