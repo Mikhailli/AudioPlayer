7a80e2565abd77bda57ddc8ecddc3a22dfc548c9
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-28 18:48:38 +0700

    feat(): add AudioViewModel

---
+++ AudioPlayer/AudioViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AudioPlayer;
+
+public class AudioViewModel
+{
+    public int Id { get; set; }
+    public int NumberInPlayList { get; set; }
+    public string Name { get; set; }
+
+    public string NameWithExtension { get; set; }
+
+    public string Path { get; set; }
+
+    public string Duration { get; set; }
+
+    public string? PreviousAudio { get; set; }
+    
+    public string? NextAudio { get; set; }
+}
