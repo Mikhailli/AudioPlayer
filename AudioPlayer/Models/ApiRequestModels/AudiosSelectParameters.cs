eb334d42162c3da5f9de25044e31aaab7bbc1342
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-28 18:43:18 +0700

    feat(models): add apiRequestModels

---
+++ AudioPlayer/Models/ApiRequestModels/AudiosSelectParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace AudioPlayer.Models.ApiRequestModels;
+
+public class AudiosSelectParameters : SelectParameters
+{
+    public AudiosSelectParameters(int start, int length, string searchPattern,
+        string? orderColumnName, bool isAscending)
+        : base(start, length, searchPattern, orderColumnName, isAscending)
+    {
+        
+    }
+}
