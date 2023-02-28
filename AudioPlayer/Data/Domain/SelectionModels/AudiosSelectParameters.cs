4e2e0cb94a52825dc2971d2d7fc31e90180394a5
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-28 18:43:41 +0700

    feat(Data): add selectionModels

---
+++ AudioPlayer/Data/Domain/SelectionModels/AudiosSelectParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace AudioPlayer.Data.Domain.SelectionModels;
+
+public class AudiosSelectParameters : SelectParameters
+{
+    public AudiosSelectParameters(int? start, int? length, string? searchPattern,
+        string orderColumnName, bool? isAscending)
+        : base(start, length, searchPattern, orderColumnName, isAscending)
+    {
+        
+    }
+}
