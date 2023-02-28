7ae8d7e3408352e0435af27e698f3e3d10d2fbd9
Mikhail <babichev.mikhail.s@gmail.com>
2023-02-28 18:45:29 +0700

    feat(): add client api and service

---
+++ AudioPlayer/Client/Services/Api.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using Flurl;
+using AudioPlayer.Models.ApiRequestModels;
+
+namespace AudioPlayer.Client.Services;
+
+public class Api
+{
+    public static class Audios
+    {
+        public static string GetAudios(string baseUrl, AudiosSelectParameters? selectParameters = null)
+        {
+            var url = new Url($"{baseUrl}/api/Home/get");
+            if (selectParameters is not null)
+            {
+                url.SetQueryParam("start", selectParameters.Start);
+                url.SetQueryParam("length", selectParameters.Length);
+                url.SetQueryParam("searchPattern", selectParameters.SearchPattern ?? "*");
+                url.SetQueryParam("orderColumnName", selectParameters.OrderColumnName);
+                url.SetQueryParam("isAscending", selectParameters.IsAscending);
+            }
+
+            return url.ToString();
+        }
+
+        public static string GetAudio(string baseUrl, int audioId) => $"{baseUrl}/api/Home/{audioId}";
+        public static string DeleteAudio(string baseUrl, int audioId) => $"{baseUrl}/api/Home/{audioId}";
+    }
+}
---
+++ AudioPlayer/Client/Services/AudiosService.cs
@@ -0,0 +1,50 @@
+﻿using AudioPlayer.Models.ApiRequestModels;
+using AudioPlayer.Models.ApiResponseModel;
+
+namespace AudioPlayer.Client.Services;
+
+public class AudiosService
+{
+    private readonly HttpClient _httpClient;
+    
+    public AudiosService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<SelectedData<AudioViewModel>> GetAudiosAsync(
+        AudiosSelectParameters? parameters = null)
+    {
+        var requestUrl = Api.Audios.GetAudios("https://localhost:44353", parameters);
+
+        var response = await _httpClient.GetAsync(requestUrl);
+
+        var selectedAudios = await response.Content.ReadAsAsync<SelectedData<AudioViewModel>>();
+        
+        return selectedAudios;
+    }
+
+    public async Task<AudioViewModel> GetAudioAsync(int audioId)
+    {
+        var requestUrl = Api.Audios.GetAudio("https://localhost:44353", audioId);
+        var response = await _httpClient.GetAsync(requestUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var audio = await response.Content.ReadAsAsync<AudioViewModel>();
+            return audio;
+        }
+        else
+        {
+            var responseString = response.Content.ReadAsStringAsync().Result;
+            throw new Exception($"Ошибка получения данных о аудио из {requestUrl} (код: {response.StatusCode}).");
+        }
+    }
+
+    public async Task DeleteAudioAsync(AudioViewModel audio)
+    {
+        var requestUrl = Api.Audios.DeleteAudio("https://localhost:44353", audio.NumberInPlayList);
+
+        var response = await _httpClient.DeleteAsync(requestUrl);
+    }
+}
