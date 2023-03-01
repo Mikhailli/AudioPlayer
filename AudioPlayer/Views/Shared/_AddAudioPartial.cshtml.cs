065eef9eaa7fa1e12cc999559d2ad38b5072855d
Mikhail <babichev.mikhail.s@gmail.com>
2023-03-01 21:48:55 +0700

    feat(views): add partialViews

---
+++ AudioPlayer/Views/Shared/_AddAudioPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AudioPlayer.Views.Shared;
+
+public class _AddAudioPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ AudioPlayer/Views/Shared/_UpdateAudioPartial.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AudioPlayer.Views.Shared;
+
+public class _UpdateAudioPartial : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
