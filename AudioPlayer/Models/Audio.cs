0899b93adddf65108d56f3a66a186989fd3dc831
Mikhail <babichev.mikhail.s@gmail.com>
2023-03-02 17:05:16 +0700

    refactor(Audio): delete useless

--- AudioPlayer/Models/Audio.cs
+++ AudioPlayer/Models/Audio.cs
@@ -2,25 +2,20 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 using AudioPlayer.Data.Domain.Interfaces;
-using AudioPlayerLibrary.Interfaces;
 
 namespace AudioPlayer.Models;
 
-public class Audio : Entity<int>, IAudio
+public class Audio : Entity<int>
 {
     public string Name { get; set; } = null!;
     public string Path { get; set; } = null!;
-   
     public int Duration { get; set; }
-    public IAudio? Previous { get; set; }
-    public IAudio? Next { get; set; }
-
-    public virtual Audio? PreviousAudio { get; set; }
-    public virtual Audio? NextAudio { get; set; }
+    public Audio? PreviousAudio { get; set; }
+    public Audio? NextAudio { get; set; }
     [JsonIgnore]
     [InverseProperty("PreviousAudio")]
-    public virtual ICollection<Audio?>? NextForPreviousAudio { get; set; }
+    public ICollection<Audio?>? NextForPreviousAudio { get; set; }
     [InverseProperty("NextAudio")]
     [JsonIgnore]
-    public virtual ICollection<Audio?>? PreviousForNextAudio { get; set; }
+    public ICollection<Audio?>? PreviousForNextAudio { get; set; }
 }
